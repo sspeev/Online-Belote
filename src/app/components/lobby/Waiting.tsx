a8374fea317a12cb9975027da131f80eb0753908
Stoyan Peev <114337044+sspeev@users.noreply.github.com>
2025-11-27 17:45:27 +0200

    Fixed design on waiting component

--- src/app/components/lobby/Waiting.tsx
+++ src/app/components/lobby/Waiting.tsx
@@ -41,20 +41,18 @@ const Waiting = () => {
     <section className="create-container h-screen relative overflow-hidden">
       <Background />
       <main className="container top-10 lg:top-10 lg:left-10 absolute mx-auto flex flex-col items-center">
-        <header className="w-full flex flex-col lg:flex-row justify-end items-center bg-gradient-to-b from-secondary-light to-secondary-dark rounded-xl p-8 mb-10">
-
-
-          <div className="flex flex-col lg:flex-row gap-3 items-center mb-6 lg:mb-0">
+        <header className="w-full flex flex-col lg:flex-row justify-center items-center bg-gradient-to-b from-secondary-light to-secondary-dark rounded-xl p-8 mb-10">
+          <div className="flex flex-col lg:flex-row gap-10 lg:gap-28 items-center mb-6 lg:mb-0">
             <div className="flex flex-col items-center justify-center">
               <h2 className="text-4xl text-center font-semibold font-default text-text-light">
                 {lobby.name}
               </h2>
               <p className="text-white text-xl font-semibold font-default capitalize">
-                {lobby.gamePhase}
+                Status: {lobby.gamePhase}
               </p>
             </div>
-            <div className="button-wrapper">
-              <div>
+            <div className="button-wrapper w-25 flex flex-row-reverse lg:flex-col-reverse justify-center gap-25 lg:gap-10">
+              <div className="button relative">
                 <Button
                   text={'Leave'}
                   liquid={true}
@@ -63,21 +61,27 @@ const Waiting = () => {
                 />
               </div>
               {playerData.player.host && (
-                <Button
-                  text={"Start"}
-                  liquid={true}
-                  onClick={handleStartGame}
-                  additionalStyles={lobby.playerCount < 4 ? 'opacity-50 pointer-events-none' : ''}
-                />
+                <div className="button relative">
+                  <Button
+                    text={'Start'}
+                    shape={BtnShape.MAIN}
+                    liquid={true}
+                    onClick={handleStartGame}
+                    additionalStyles={
+                      lobby.playerCount < 4
+                        ? 'opacity-50 pointer-events-none'
+                        : ''
+                    }
+                  />
+                </div>
               )}
             </div>
           </div>
 
-
-          <div className="divider hidden lg:block h-30 w-px bg-white/30 mx-3"></div>
-          <div className="text-center text-text-light text-xl lg:text-4xl font-semibold font-default max-w-lg">
-            BELOTE ENGINE STAGE DEVELOPMENT
-          </div>
+            <div className="divider block w-50 lg:h-30 h-px lg:w-px bg-white/30 my-5 lg:mx-3"></div>
+            <div className="text-center text-text-light text-xl lg:text-4xl font-semibold font-default max-w-lg">
+              BELOTE ENGINE STAGE DEVELOPMENT
+            </div>
         </header>
         {/*<PlayerList />*/}
         <section className="text-sm text-white/70 mt-6">
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,29 +1,11 @@
 import { createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import Layout from '../app/Layout'
 
 type RouterContext = {};
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
-    <>
       <Layout />
-      <TanStackDevtools
-        config={{
-          defaultOpen: false,
-          openHotkey: ['Shift', 'A'],
-          position: 'bottom-left',
-          theme: 'dark'
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
   ),
   errorComponent: ({ error }) => <div>{`Root route error: ${error.message}`}</div>,
   notFoundComponent: () => <div>Root route not found</div>,
